7224a321cd40d27b3f9d656ffe0c65356e9ba62e
Mateusz Mackowiak <darkstaron123@gmail.com>
2022-04-26 09:35:23 +0200

    chyba zrobilem lab6.2

--- Laboratorium 6/Zad1/backend/server.js
+++ Laboratorium 6/Zad1/backend/server.js
@@ -1,15 +1,11 @@
-'use strict';
-
-const express = require('express');
-
-const app = express();
-
-const PORT = process.env.PGPORT;
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+  res.send('Hello World!')
+})
 
-app.listen(PORT, () => {
-  console.log('Listening on port 5000');
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
